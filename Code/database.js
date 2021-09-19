6bf69c2b242a623ef7f5e8ea0f3c496a2efb69cb
Gaelle <gaelle.boyard@gmail.com>
2021-09-19 13:52:12 +0200

    Link between js and the database
    Please do not update line 7 to 10 on git, only on your computer. We probably have different password and user names.

---
+++ Code/database.js
@@ -0,0 +1,21 @@
+const mysql=require('mysql');
+
+const dbCon = mysql.createConnection({
+    //The password and user might be different for you. I do not have a password that's why it is in comment.
+    //Please only change this part locally, otherwise we'll all have to update it at each git pull.
+    //password :'',
+    user : 'root',
+    host : 'localhost',
+    port : '3306',
+    database : 'electric'
+    
+});
+
+dbCon.connect(function(error){
+
+    //query SQL
+    if(error) throw error;
+    console.log('Database connected successfully !');
+});
+
+module.export = dbCon;
