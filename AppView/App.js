f7a9a9adfec6a545aca5065053375e5b23dce439
Ebba00 <ebba.molinder@me.com>
2021-09-25 01:22:12 +0200

    Beginning of a Map
    Some work on a google map with created API Keys. May have misunderstood and only need one API Key but that's not a problem now.

--- AppView/App.js
+++ AppView/App.js
@@ -1,7 +1,32 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import MapView, {PROVIDER_GOOGLE} from 'react-native-maps';
+HAVE_GOOGLE_MAPS=1
+// API Key IOS Map
+// AIzaSyArsdMW-uQ3t87BoHnwgY_pqTyoEyC_fjM
 
+// API Key Android Map
+// AIzaSyDrXtMBPlOkP785ixYJ0D606ROWXvqKK40
+/*
+export default class App extends Component<Props> {
+    render() {    
+      return (      
+        <MapView   
+        provider ={PROVIDER_GOOGLE}     
+        style={{flex: 1}}         
+        region={{          
+          latitude: 42.882004,          
+          longitude: 74.582748,          
+          latitudeDelta: 0.0922,          
+          longitudeDelta: 0.0421        
+        }}        
+        showsUserLocation={true}      
+      />    
+    );  
+  }
+}
+**/
 export default function App() {
   return (
     <View style={styles.container}>
